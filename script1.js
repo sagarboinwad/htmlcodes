d4f44b364a2fc6be7ca857686284a375c9acf189
Sagar Vikas Boinwad <sagarboinwad74@gmail.com>
2025-02-15 14:11:56 +0530

    Create script1.js

---
+++ script1.js
@@ -0,0 +1,10 @@
+let elements = document.getElementsByClassName("light");
+for (let i = 0; i < elements.length; i++) {
+    elements[i].style.color = "green";
+}
+
+
+function changecolor () {
+  document.getElementById("normal").style.color = "red";
+}
+
